ad59f7545714107576e057e3f68b26099da48b80
naveen kumar <naveenkumarcse2018@gmail.com>
2021-03-09 16:29:01 +0530

    Examples

--- Basic Programs/001-Hello-World.go
+++ Basic Programs/001-Hello-World.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hellow,World!...")
+	fmt.Println("This is my first go programming code!.");
+	for i := 0; i < 5; i++ {
+		fmt.Println("Naveen Loves coding");
+	}
+	otherFunction()
+	fmt.Println("\n This is after function call");
+}
+func otherFunction() {
+	fmt.Println("\n We are in other Function call. We can use semicolons at the end of the statement as well");
 }
--- Basic Programs/002-Second-Example.go
+++ Basic Programs/002-Second-Example.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Naveen Kumar Kammari")
+	n_bytes,error := fmt.Println("Naveen Kumar kammari",12,23)
+	fmt.Println("No.of bytes: ",n_bytes)
+	fmt.Println("Error code: ",error)
+	//Go won't allow unused variables
+	// _ will be used for unused variables
+	// n,_:= fmt.Println("Naveen",12,true)
+	//fmt.Println(n)
+	//Here we can't worry about the _ variable
 }
